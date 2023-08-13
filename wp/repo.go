72ae33c79bf2a72e99c328da099b0c175a2b993c
Robert Kaussow <mail@thegeeklab.de>
2023-08-13 13:56:07 +0200

    refactor: rename package from plugin to wp

--- wp/repo.go
+++ wp/repo.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package plugin
+package wp
 
 import (
 	"github.com/urfave/cli/v2"
--- wp/types_test.go
+++ wp/types_test.go
@@ -1,4 +1,4 @@
-package plugin
+package wp
 
 import (
 	"reflect"

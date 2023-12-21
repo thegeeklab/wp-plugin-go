4d621987403cff839c062fae2002573402c99dc7
Robert Kaussow <mail@thegeeklab.de>
2023-12-21 09:13:00 +0100

    feat: add helper to auto-generate cli docs (#40)

---
+++ docs/docs.go
@@ -0,0 +1,99 @@
+package docs
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"sort"
+	"strings"
+
+	"github.com/urfave/cli/v2"
+)
+
+type PluginArg struct {
+	Name        string
+	EnvVars     []string
+	Description string
+	Default     string
+}
+
+type CliTemplate struct {
+	Name        string
+	Version     string
+	Description string
+	Usage       string
+	UsageText   string
+	GlobalArgs  []*PluginArg
+}
+
+//go:embed templates
+var templateFs embed.FS
+
+// ToMarkdown creates a markdown string for the `*App`
+// The function errors if either parsing or writing of the string fails.
+func ToMarkdown(app *cli.App) (string, error) {
+	var w bytes.Buffer
+
+	tpls, err := template.New("cli").ParseFS(templateFs, "**/*.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	if err := tpls.ExecuteTemplate(&w, "markdown.md.tmpl", GetTemplateData(app)); err != nil {
+		return "", err
+	}
+
+	return w.String(), nil
+}
+
+func GetTemplateData(app *cli.App) *CliTemplate {
+	return &CliTemplate{
+		Name:        app.Name,
+		Version:     app.Version,
+		Description: prepareMultilineString(app.Description),
+		Usage:       prepareMultilineString(app.Usage),
+		UsageText:   prepareMultilineString(app.UsageText),
+		GlobalArgs:  prepareArgsWithValues(app.VisibleFlags()),
+	}
+}
+
+func prepareMultilineString(s string) string {
+	return strings.TrimRight(
+		strings.TrimSpace(
+			strings.ReplaceAll(s, "\n", " "),
+		),
+		".\r\n\t",
+	)
+}
+
+func prepareArgsWithValues(flags []cli.Flag) []*PluginArg {
+	return parseFlags(flags)
+}
+
+func parseFlags(flags []cli.Flag) []*PluginArg {
+	args := make([]*PluginArg, 0)
+
+	for _, f := range flags {
+		flag, ok := f.(cli.DocGenerationFlag)
+		if !ok {
+			continue
+		}
+
+		modArg := &PluginArg{}
+
+		name := flag.GetEnvVars()[0]
+		name = strings.TrimPrefix(name, "PLUGIN_")
+		modArg.Name = strings.ToLower(strings.TrimSpace(name))
+
+		modArg.Description = flag.GetUsage()
+		modArg.Default = flag.GetDefaultText()
+
+		args = append(args, modArg)
+	}
+
+	sort.SliceStable(args, func(i, j int) bool {
+		return args[i].Name < args[j].Name
+	})
+
+	return args
+}
---
+++ docs/testdata/expected-doc-full.md
@@ -0,0 +1,13 @@
+# test
+
+test description
+
+## Parameters
+
+**_dummy_flag_** (defaut: &#34;test&#34;)\
+&emsp;dummy flag desc
+
+**_x_simple_flag_**
+
+**_z_other_flag_**\
+&emsp;other flag with desc

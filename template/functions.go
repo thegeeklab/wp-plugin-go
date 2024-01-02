19ad781691daee0039e1afbade927577eb4ad50a
Robert Kaussow <mail@thegeeklab.de>
2024-01-02 21:51:07 +0100

    feat: add template func ToSentence

--- template/functions.go
+++ template/functions.go
@@ -1,14 +1,19 @@
 package template
 
 import (
+	"fmt"
 	"text/template"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/Masterminds/sprig/v3"
 )
 
 func LoadFuncMap() template.FuncMap {
 	sprigFuncs := sprig.GenericFuncMap()
-	customFuncs := template.FuncMap{}
+	customFuncs := template.FuncMap{
+		"ToSentence": ToSentence,
+	}
 
 	for name, f := range customFuncs {
 		if _, ok := sprigFuncs[name]; ok {
@@ -19,4 +24,14 @@ func LoadFuncMap() template.FuncMap {
 	}
 
 	return sprigFuncs
+}
+
+func ToSentence(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	r, n := utf8.DecodeRuneInString(s)
+
+	return fmt.Sprintf("%s%s.", string(unicode.ToUpper(r)), s[n:])
 }

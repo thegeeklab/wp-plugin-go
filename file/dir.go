f451acea44a9b4a9dd5ff24971f08097ff73e3da
Robert Kaussow <mail@thegeeklab.de>
2024-05-05 16:31:30 +0200

    feat: add new helper function IsDirEmpty

--- file/dir.go
+++ file/dir.go
@@ -1,6 +1,8 @@
 package file
 
 import (
+	"errors"
+	"io"
 	"os"
 )
 
@@ -25,6 +27,27 @@ func IsDir(path string) (bool, error) {
 
 	if os.IsNotExist(err) {
 		return false, nil
+	}
+
+	return false, err
+}
+
+// IsDirEmpty returns whether the given directory path is empty.
+// If the path does not exist or is not a directory, it returns (false, err).
+func IsDirEmpty(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdir(1)
+	if err == nil {
+		return true, nil
+	}
+
+	if errors.Is(err, io.EOF) {
+		return true, nil
 	}
 
 	return false, err
